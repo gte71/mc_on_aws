1532579c97ef38da6887b8450286eff4ddf5c4af
gte71 <g.teljstedt@gmail.com>
2020-12-27 21:02:04 +0100

    fixed again name of spigot..jar file

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-SPIGOT_JAR=${MC}/spigot*.jar
+SPIGOT_JAR=./spigot*.jar
 screen -d -m -S "minecraft" java -Xms1024M -Xmx1524M -XX:+UseG1GC  -Dcom.mojang.eula.agree=true  -jar ${SPIGOT_JAR} nogui
