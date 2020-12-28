ab8a219c7390e6b7f25148c9683e2ec9673e9279
gte71 <g.teljstedt@gmail.com>
2020-12-28 13:17:12 +0100

    fixes

--- supervisor.sh
+++ supervisor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-START=./start.sh
+START=/home/minecraft/world1/start.sh
 ME=`basename "$0"`
 PIDFILE=$ME.pid
 rm -f $PIDFILE
 echo $$ > $PIDFILE
 while [[ -f "$PIDFILE" ]]
 do
-cnt=`ps -ef | grep -v grep  | grep -e '^ec2-user[[:space:]][0-9][0-9]*[[:space:]][0-9][0-9]*.*java.*spigot' | wc -l`
+cnt=`ps -ef | grep -v grep  | grep -e '^ec2-user[[:space:]]+[0-9][0-9]*[[:space:]]+[0-9][0-9]*.*[0-9][0-9]:[0-9][0-9]:[0-9][0-9][[:space:]]+java.*spigot' | wc -l`
 if [[ $cnt -eq 0 ]];then
 	echo count is $cnt , start spigot
 	$START
